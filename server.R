707fcbeba3b106b246eb1835b19be851958d58f6
awdye <alex.w.dye@gmail.com>
2014-10-16 12:50:38 -0400

    Almost there, but not quite. Keep getting errors "figure margins too large" and "x and y lengths differ."  Not sure how to solve these issues, but I think the basic code is right.

--- server.R
+++ server.R
@@ -1,38 +1,45 @@
-#setwd ("C:/shiny/pc1scores")
+#setwd ("C:/WVU/shinyThings")
 library(dplR)
 
 # Define server logic required to draw a chron_plot
 shinyServer(function(input, output) {
   
   #read in data
-  pc1_scores = read.table ("PC1_scores.txt", header=TRUE)
-  year = pc1_scores$year
+  pc1_scores = read.table ("Pc_scores.txt", header=TRUE)
+  year = as.numeric(pc1_scores$year)
   pc1_score = pc1_scores$PC1_score
-  spline_15 = pc1_scores$spline_15
-  total_sd = pc1_scores$total_sd
+  total_sd = as.numeric(pc1_scores$total_sd)
   
   #create an action button for spline stiffness
-  pc1_spl = ffcsaps(pc1_scores$PC1_score, pc1_scores$year, nyrs= input$button1) #alex- not sure if button1 is right also do we 
-  need to define range of values: 5, 12, 25, 51, 101 would be ideal, or user inputs?
+  pc1_spl5 = ffcsaps(pc1_score, year, nyrs= 5) 
+  pc1_spl15 = ffcsaps(pc1_score, year, nyrs= 15)
+  pc1_spl25 = ffcsaps(pc1_score, year, nyrs= 25)
+  pc1_spl51 = ffcsaps(pc1_score, year, nyrs= 51)
+  pc1_spl101 = ffcsaps(pc1_score, year, nyrs= 101)
+  klp_std = pc1_scores$klp_std
+  ulp_std = pc1_scores$ulp_std
   
   output$chron_plot <- renderPlot({
     paste("you have selected", input$slider1)
+    #spline = ffcsaps(pc1_score, year, nyrs = input$numeric1)
     layout(matrix(c(1,2), byrow=TRUE), widths=c(8,8), heights=c(3,1), TRUE)
     
     #pc scorers plot
     plot(year,pc1_score,type="l", col="grey30", xaxt="n", axes=FALSE, xlim = input$slider1, 
-         ylim=c(0,2), cex.lab=0.75, ylab="", xlab="", lwd = 2.0, cex.axis=0.75)
-    lines(year,spline_15, col="grey30", lwd = 3),
+         ylim=c(-3,3), cex.lab=0.75, ylab="", xlab="", lwd = 2.0, cex.axis=0.75)
+    lines(year, input$button1, col="grey30", lwd = 3)
+    lines(year, input$button2, col="red", lwd = 3)
     
     # sample depth plot 
-    plot(year, total_sd, col='grey', type='l', ylab="", xlim = input$slider1, ylim=1.5*c(0, max(total_sd)))
+    plot(year, total_sd, col='grey30', type='l', ylab="", xlim = input$slider1, 
+         ylim=1.5*c(0, max(total_sd)))
     axis(1,at=seq(0,2000, by=50))
   })
-       
-  })
   
-  
-  
+})
+
+
+
 
 
 
@@ -49,3 +56,4 @@ shinyServer(function(input, output) {
 # name="awdgeog",
 #token="753D87E9E7F7EE199759BBEE2DC41512",
 #secret="NeidIKFVD7pfnvHGeXGWtQA6XXqiAltvWgKqG72n")
+
--- ui.R
+++ ui.R
@@ -1,9 +1,14 @@
+#ui:
+  
 library(shiny)
+library(dplR)
 
-#setwd ("C:/shiny/pc1scores")
+#setwd ("C:/WVU/shinyThings")
 
 
-# Define UI for application that draws a histogram
+# Define variables
+
+
 shinyUI(fluidPage(
   
   
@@ -11,23 +16,28 @@ shinyUI(fluidPage(
   titlePanel("pc1Scores"),
   
   
-  # Sidebar with a slider input 
-  sidebarLayout(
+  
+  #Sidebar with a slider input 
+  #sidebarLayout(
     sidebarPanel(
       sliderInput("slider1",
                   "Adjust x max values",
                   min = 79,
                   max = 2011,
-                  value = c(733,896))
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
+                  value = c(733,896)),
       
-      
-      plotOutput("chron_plot"),
-      
-    )
-    
-  )
-))
+      #numericInput("numeric1",
+                  # "Choose spline:",
+                  # 15,
+                 #  min = 1,
+                 #  max = 101),
+      radioButtons(inputId = "button1", 
+                   label = "adjust spline", 
+                   choices = c("5"="pc1_spl5", "15"="pc1_spl15", "25"="pc1_spl25", "51"="pc1_spl51", 
+                               "101"="pc1_spl101")
+                   ,selected = "pc1_spl15"),
+      radioButtons("button2",
+                   "std chronologies",
+                   c("klp"="klp_std", "ulp"="ulp_std")),
+    mainPanel(plotOutput("chron_plot")))
+        ))
